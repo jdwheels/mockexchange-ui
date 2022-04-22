0f90fc04f3785a0be341a4c450de9244a1b5440e
jdwheels <Wheeler.John.D@gmail.com>
2022-04-22 08:43:44 -0400

    Add cloudfront upload

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App: FC = function () {
   }, [setUserDetails]);
   return (
     <div>
+      <div>test</div>
       {userDetails ? (
         <button className="logout" type="button" onClick={handleLogout}>Logout</button>
       ) : (
