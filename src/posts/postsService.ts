97de17ed2f0a55ee80cd3e6e0cfb4828bfd83f65
jdwheels <Wheeler.John.D@gmail.com>
2022-05-07 12:23:46 -0400

    Update title

--- src/posts/postsService.ts
+++ src/posts/postsService.ts
@@ -1,4 +1,4 @@
-import { MockPost, PostsCollectionResponse, PostsResponse } from './types';
+import { MockPost, PostsResponse } from './types';
 import { PageOptions } from '../common/types';
 
 const baseUrl = '/posts-api';
@@ -12,13 +12,12 @@ async function getQuestions(
 }
 
 async function getAnswers(questionId: number | string): Promise<MockPost[]> {
-  const response = await fetch(`${baseUrl}/mockPosts/search/getAnswers?parentId=${questionId}`);
-  const body = await response.json() as PostsCollectionResponse;
-  return body._embedded.mockPosts;
+  const response = await fetch(`${baseUrl}/posts/answers?parentId=${questionId}`);
+  return await response.json() as MockPost[];
 }
 
 async function getQuestion(id: number | string): Promise<MockPost> {
-  const response = await fetch(`${baseUrl}/mockPosts/${id}`);
+  const response = await fetch(`${baseUrl}/posts/questions/${id}`);
   return await response.json() as MockPost;
 }
 
